4f5234ed6e29fc65d97e6c99cede7fa354efefd2
BroukPytlik <github@tulak.com>
2013-03-01 07:44:30 +0100

    Some more changes...

--- www/index.php
+++ www/index.php
@@ -3,6 +3,8 @@
 // Uncomment this line if you must temporarily take down your site for maintenance.
 // require '.maintenance.php';
 
+define('DATA_ROOT',__DIR__);
+
 // Let bootstrap create Dependency Injection container.
 $container = require __DIR__ . '/../app/bootstrap.php';
 
