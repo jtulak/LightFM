8c081ac3b0c2673ca6297bda0fbf7a04b95e8bae
BroukPytlik <github@tulak.com>
2013-03-01 18:32:26 +0100

    createPath splitted to more functions

--- app/model/Files/IO.php
+++ app/model/Files/IO.php
@@ -8,51 +8,154 @@
  * For the full copyright and license information, please view
  * the file license.txt that was distributed with this source code.
  */
+
 namespace LightFM;
 
 /**
  * 
  * 
  */
-abstract class IO extends \Nette\Object{
+abstract class IO extends \Nette\Object {
+
+    /**
+     * 	Smarter replacement of php is_file - go through symlinks
+     * @param string $path
+     * @return boolean
+     */
+    public static function is_file($path) {
+	// at first skim through links
+	if (is_link($path))
+	    while (is_link($path = readlink($path)));
+
+	if (is_file($path))
+	    return true;
+
+	return false;
+    }
+
+    /**
+     * 	Smarter replacement of php is_dir - go through symlinks
+     * @param string $path
+     * @return boolean
+     */
+    public static function is_dir($path) {
+	// at first skim through links
+	if (is_link($path))
+	    while (is_link($path = readlink($path)));
+
+	if (is_dir($path))
+	    return true;
+
+	return false;
+    }
+
+    /**
+     * 
+     * @param string $restOfPath
+     * @return type
+     */
+    private static function createPath_explodePath($restOfPath) {
+	// remove slash at the end
+	if (substr($restOfPath, -1, 1) == '/')
+	    $restOfPath = substr($restOfPath, 0, -1);
+
+	// split the path to the actual node and to the rest
+	if (strpos($restOfPath, '/') === FALSE) {
+	    // there is no slash - so simply set
+	    $dir = $restOfPath;
+	    $restOfPath = "";
+	} else if (substr($restOfPath, 0, 1) == '/') {
+	    // there is a slash at the begining - so simply set 
+	    $dir = "/";
+	    $restOfPath = substr($restOfPath, 1);
+	} else {
+	    // there is a slash in the middle - split it
+	    list($dir, $restOfPath) = explode('/', $restOfPath, 2);
+	    $restOfPath = trim($restOfPath, '/');
+	}
+	return array($dir, $restOfPath);
+    }
 
     
     /**
-     *	Smarter replacement of php is_file - go through symlinks
-     * @param string $path
-     * @return boolean
+     * 
+     * @param string $fullPath
+     * @return \LightFM\Directory
      */
-    public static function is_file($path){
-	// at first skim through links
-	if(is_link($path))
-	    while(is_link($path = readlink($path)));
-	
-	if (is_file($path))
-	    return true;
-	
-	return false;
+    private static function createPath_tryCreate_final($fullPath) {
+	return new \LightFM\Directory($fullPath);
     }
-    
-    
+
     /**
-     *	Smarter replacement of php is_dir - go through symlinks
-     * @param string $path
-     * @return boolean
+     * 
+     * @param string $fullPath
+     * @param string $fullDir
+     * @return \LightFM\Image|\LightFM\File
      */
-    public static function is_dir($path){
-	// at first skim through links
-	if(is_link($path))
-	    while(is_link($path = readlink($path)));
-	
-	if (is_dir($path))
-	    return true;
-	
-	return false;
-	
-	
+    private static function createPath_tryCreate_file($fullPath, $fullDir) {
+	if (\LightFM\Filetypes::isImage(DATA_ROOT . $fullPath)) {
+	    return new \LightFM\Image($fullDir);
+	} else {
+	    return new \LightFM\File($fullDir);
+	}
     }
-    
-    
+
+    /**
+     * 
+     * @param string $fullPath
+     * @param string $fullDir
+     * @param string $restOfPath
+     * @param \LightFM\DirConfig $config
+     * @return \LightFM\Directory
+     */
+    private static function createPath_tryCreate_folder($fullPath, $fullDir, $restOfPath, \LightFM\DirConfig $config) {
+	$created = new \LightFM\Directory($fullDir);
+
+	// recursively create rest of the path
+	$created->usedChild = self::createPath($fullPath, $restOfPath, $config);
+	$created->usedChild->parent = $created;
+
+	// save a child config for case of emptying of $created
+	$childConf = $created->usedChild->config;
+	$childHidden = $created->usedChild->hidden;
+	if ($created->usedChild->dummy) {
+	    // if the subdir is blacklisted, replace by empty node
+	    $created = new \LightFM\Directory(NULL);
+	}
+
+	// copy the needs password from the child
+	$created->password = $childConf->getAccessPassword();
+	$created->hidden = $childHidden;
+
+	return $created;
+    }
+
+    /**
+     * 
+     * @param string $fullPath
+     * @param string $fullDir
+     * @param string $restOfPath
+     * @param \LightFM\DirConfig $config
+     * @return \LightFM\Directory
+     */
+    private static function createPath_tryCreate($fullPath, $fullDir, $restOfPath, \LightFM\DirConfig $config) {
+
+	if ($restOfPath == "") {
+	    // if the $restOfPath is empty - then we are creating path 
+	    // thats end in this directory
+	    $created = self::createPath_tryCreate_final($fullPath);
+	} else if (strpos('/', $restOfPath) === FALSE && self::is_file($restOfPath)) {
+	    // the end of the path is a file in this dir
+	    // FIXME chain of symlinks to a directory interpreted as a file
+	    $created = self::createPath_tryCreate_file($fullPath, $fullDir);
+	} else {
+	    // this dir is not the final node, there is a subdir
+	    // create node for this dir
+	    $created = self::createPath_tryCreate_folder($fullPath, $fullDir, $restOfPath, $config);
+	}
+	return $created;
+    }
+
     /**
      * 
      * @param string $fullPath	    full path relatively to the data root
@@ -63,31 +166,15 @@ abstract class IO extends \Nette\Object{
     public static function createPath($fullPath, $restOfPath, \LightFM\DirConfig $parentsConfig = NULL) {
 	// Remove slashes from begining and end (if any)
 	// and get top dir from the path
-	
-	// remove slash at the end
-	if(substr($restOfPath,-1,1) == '/') $restOfPath = substr($restOfPath,0,-1);
-	
-	// split the path to the actual node and to the rest
-	if(strpos($restOfPath, '/') === FALSE){
-	    // there is no slash - so simply set
-	    $dir = $restOfPath;
-	    $restOfPath="";
-	}else if(substr($restOfPath,0,1) == '/'){
-	    // there is a slash at the begining - so simply set 
-	    $dir="/";
-	    $restOfPath = substr($restOfPath,1);
-	}else{
-	    // there is a slash in the middle - split it
-	    list($dir, $restOfPath) = explode('/', $restOfPath, 2);
-	    $restOfPath = trim($restOfPath, '/');
-	}
-	
+
+	list($dir, $restOfPath) = self::createPath_explodePath($restOfPath);
+
 	// create path to this dir - remove the rest from the full path to get 
 	// path to this dir
 	$restLen = strlen($restOfPath);
-	if($restLen){
-	    $fullDir = substr($fullPath,0,-($restLen+1));
-	}else{
+	if ($restLen) {
+	    $fullDir = substr($fullPath, 0, -($restLen + 1));
+	} else {
 	    $fullDir = $fullPath;
 	}
 	$config = new \LightFM\DirConfig($fullDir);
@@ -95,69 +182,29 @@ abstract class IO extends \Nette\Object{
 	    // load config
 	    // and inherite
 	    $config->inherite($parentsConfig);
-	    
-	    if ($restOfPath == "") {
-		// if the $restOfPath is empty - then we are creating path 
-		// thats end in this directory
-		$created = new \LightFM\Directory($fullPath);
-		
-	    } else if (strpos('/', $restOfPath) === FALSE && self::is_file($restOfPath)) {
-		// the end of the path is a file in this dir
-		// FIXME chain of symlinks to a directory interpreted as a file
 
-		if (\LightFM\Filetypes::isImage(DATA_ROOT . $fullPath)) {
-		    $created = new \LightFM\Image($fullDir);
-		} else {
-		    $created = new \LightFM\File($fullDir);
-		}
-	    } else {
-		// this dir is not the final node, there is a subdir
-		// create node for this dir
-		$created = new \LightFM\Directory($fullDir);
-		
-		// recursively create rest of the path
-		$created->usedChild = self::createPath($fullPath, $restOfPath,$config);
-		$created->usedChild->parent = $created;
-		
-		// save a child config for case of emptying of $created
-		$childConf =  $created->usedChild->config;
-		$childHidden = $created->usedChild->hidden;
-		if ($created->usedChild->dummy) {
-		    // if the subdir is blacklisted, replace by empty node
-		    $created = new \LightFM\Directory(NULL);
-		}
-		
-		// copy the needs password from the child
-		$created->password = $childConf->getAccessPassword();
-		$created->hidden=$childHidden;
-	    }
-
-	    
-	    
+	    $created = self::createPath_tryCreate($fullPath, $fullDir, $restOfPath, $config);
 	} catch (\Nette\FileNotFoundException $e) {
-		// file not found - create empty node
-		$created = new \LightFM\Directory(NULL);
+	    // file not found - create empty node
+	    $created = new \LightFM\Directory(NULL);
 	}
 
 	if (empty($created->password)) {
 	    // if subdirs do not needs a password, check this one
 	    $created->password = $config->getAccessPassword();
 	}
-	
-	if ($config->isBlacklisted($dir) || 
-		    $config->isBlacklisted(DATA_ROOT.$fullDir)) {
-		// if the file/dir is blacklisted, replace by empty node
-		$created = new \LightFM\Directory(NULL);
-	    }
-	
+
+	if ($config->isBlacklisted($dir) ||
+		$config->isBlacklisted(DATA_ROOT . $fullDir)) {
+	    // if the file/dir is blacklisted, replace by empty node
+	    $created = new \LightFM\Directory(NULL);
+	}
+
 	// assign the config for this directory
 	$created->config = $config;
 
 
 	return $created;
     }
-    
-    
-    
-    
+
 }
