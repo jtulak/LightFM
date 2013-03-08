33aac982a39f86f017e8691ea0c80223843d9449
BroukPytlik <github@tulak.com>
2013-03-08 12:18:28 +0100

    added comments

--- app/presenters/IDirectoryPresenter.php
+++ app/presenters/IDirectoryPresenter.php
@@ -1,16 +1,19 @@
 <?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * This file is part of LightFM web file manager.
+ * 
+ * Copyright (c) 2013 Jan Tulak (http://tulak.me)
+ * 
+ * For the full copyright and license information, please view
+ * the file license.txt that was distributed with this source code.
  */
-
 /**
  *
- * @author dragon
+ * @author Jan Tulak
  */
 interface IDirectoryPresenter {
-    //put your code here
+    
+    
 }
 
 
