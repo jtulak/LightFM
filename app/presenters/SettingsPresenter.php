82a5542dbd280c0e85bc841e361b62fbc13e361c
BroukPytlik <github@tulak.com>
2013-03-11 21:26:53 +0100

    Created basic form for settings (still not working) and changed look of settings site

--- app/presenters/SettingsPresenter.php
+++ app/presenters/SettingsPresenter.php
@@ -49,7 +49,7 @@ class SettingsPresenter extends BasePresenter {
      */
      protected function createComponentAccessPassword($name)
     {
-	// TODO only for dump! 
+	// TODO only for dump of password! 
 	$tested = $this->viewed;
 	while($tested !== NULL && empty($tested->Password)) {
 	    // try to find the clossest password
@@ -57,8 +57,6 @@ class SettingsPresenter extends BasePresenter {
 	}
        
        
-       // TODO detect logged password also in subdirs
-       // TODO redirecting to the page where user was
 	 
         $form = new Nette\Application\UI\Form($this, $name);
 	
@@ -95,6 +93,51 @@ class SettingsPresenter extends BasePresenter {
 	}
         //$this->syntax = $values->syntax;
 	//
+    }
+    
+    /**
+     * Create form for language selection
+     * @param type $name
+     * @return \Nette\Application\UI\Form
+     */
+     protected function createComponentSettingsForm($name)
+    {
+        $form = new Nette\Application\UI\Form($this, $name);
+	
+	$form->addGroup('Options for folder "'.'');//$this->viewed->Name.'"');
+	$form->addText('accessPassword', 'Access Password');
+	
+	$form->addGroup('General options');
+	$form->addCheckbox('hiddenFiles','Show hidden files');
+	
+	$form->addPassword('userPassword','Your new password:');
+	$form->addPassword('userPasswordVerify','Your new password again:')
+		->addConditionOn($form['userPassword'],Nette\Application\UI\Form::FILLED)
+		->addRule(Nette\Application\UI\Form::EQUAL, 'Password missmatch', $form['userPassword']);
+	
+	$form->addSubmit('submit','Submit');
+	
+        $form->onSuccess[] = callback($this, 'settingsFormSubmitted');
+        return $form;
+    }
+
+    /** called after selection submit */
+    public function settingsFormSubmitted(Nette\Application\UI\Form $form)
+    {
+	$tested = $this->viewed;
+	while($tested !== NULL && empty($tested->Password)) {
+	    // try to find the clossest password
+	    $tested = $tested->Parent;
+	}
+	
+        $values = $form->getValues();
+	
+	if($values->accessPassword == $tested->Password){
+	    Authenticator::confirmAccess($values->accessPassword , $tested->Path, $values->remember);
+	    $this->redirectUrl($this->req);
+	}else{
+	    $form['accessPassword']->addError('Invalid password!');
+	}
     }
 }
 
