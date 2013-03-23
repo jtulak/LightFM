b4c07ab576d3e220fb65164b6c12363d59cdcf79
BroukPytlik <github@tulak.com>
2013-03-23 15:57:02 +0100

    Click on item in list/gallery now change the  selection

--- www/filemanager/js/galleryView.js
+++ www/filemanager/js/galleryView.js
@@ -9,6 +9,15 @@ $(function() {
     });
     
     //computeSizes();
+    
+    
+    /* Click on item changes the selection */
+    $("section.folder,section.image").click(function(event) {
+	event.preventDefault();
+	$(this).find('input[type=checkbox]').click();
+    }).find("a,input").click(function(e) {
+	e.stopPropagation();
+    });
 });
 
 
--- www/filemanager/js/listView.js
+++ www/filemanager/js/listView.js
@@ -7,16 +7,24 @@ $(function() {
     sidebarOnChangeDuring.push(function() {
 	nameWidthResize();
     });
-    
+
     //computeSizes();
+
+    /* Click on item changes the selection */
+    $("section.folder,section.file").click(function(event) {
+	event.preventDefault();
+	$(this).find('input[type=checkbox]').click();
+    }).find("a,input").click(function(e) {
+	e.stopPropagation();
+    });
 });
 
-function computeSizes(){
-    
-$("#outer-box").height( $(window).height() -1);
-$("#inner-box").height($(window).height()-$("#header").height()-$("#footer").height()-2*parseInt($("#inner-box").css('padding'))-5)
+function computeSizes() {
 
-$("#data").height($("#inner-box").height()-($("#data").offset().top-$("#inner-box").offset().top));
+    $("#outer-box").height($(window).height() - 1);
+    $("#inner-box").height($(window).height() - $("#header").height() - $("#footer").height() - 2 * parseInt($("#inner-box").css('padding')) - 5)
+
+    $("#data").height($("#inner-box").height() - ($("#data").offset().top - $("#inner-box").offset().top));
 }
 
 function nameWidthResize() {
